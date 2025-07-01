a85233e757e903d4227a81b27d83ca56f8055961
John Kennady_official <j.johny.111@gmail.com>
2025-07-01 04:43:42 +0000

    why am i still not able to publish, am getting a error saying something

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,16 @@
-// A basic service worker for PWA functionality
+// This is a basic, no-op service worker that allows the app to be installed.
 
-const CACHE_NAME = 'cloudstage-cache-v1';
-const urlsToCache = [
-  '/',
-  '/styles/globals.css',
-  // Add other important assets here.
-  // Note: This is a basic example. A production-ready SW would be more complex.
-];
-
-self.addEventListener('install', (event) => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  // Immediately activate the new service worker.
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('activate', event => {
+  console.log('Service worker activating.');
 });
 
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+// A basic fetch handler to make the app installable.
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
