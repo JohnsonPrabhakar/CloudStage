f37aeacb9663f0aa89e79c6ab62e25cb5b8f77fb
John Kennady_official <j.johny.111@gmail.com>
2025-07-09 08:33:27 +0000

    no no even now after publishing i see that there is now updated function

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Forcing a fresh build on publish: 2024-07-26T14:00:00Z
+  // Forcing a fresh build on publish: 2024-07-26T22:00:00Z
   typescript: {
     ignoreBuildErrors: true,
   },
